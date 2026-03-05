c8513dcbc1c26dd7fa114d490e719782b1c6db4a
KAPKEPOT <185211085+KAPKEPOT@users.noreply.github.com>
2026-03-05 14:27:10 +0300

    Refactor Eiten class and update imports

--- eiten.py
+++ eiten.py
@@ -11,7 +11,7 @@ from strategy_manager import StrategyManager
 
 class Eiten:
     def __init__(self, args):
-        plt.style.use('seaborn-white')
+        plt.style.use('seaborn-v0_8-white')
         plt.rc('grid', linestyle="dotted", color='#a0a0a0')
         plt.rcParams['axes.edgecolor'] = "#04383F"
         plt.rcParams['figure.figsize'] = (12, 6)
@@ -269,3 +269,4 @@ class Eiten:
         plt.xlabel("Symbols", fontsize=14)
         plt.ylabel("Weight in Portfolio", fontsize=14)
         plt.title("Portfolio Weights for Different Strategies", fontsize=14)
+
